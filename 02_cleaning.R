830798388f10083a4a425a0eb81793d2dd2b4638
mikegibson93 <45637415+mikegibson93@users.noreply.github.com>
2020-06-12 00:41:38 -0400

    saving cleaning changes -- need to circle back once reading is complete

--- 02_cleaning.R
+++ 02_cleaning.R
@@ -5,8 +5,18 @@ library('tidyr')
 library('rvest')
 library('ggplot2')
 library('tidyverse')
+library('sjmisc')
 
 
 setwd("/Users/michaelgibson/Documents/GitHub/Conde-Naste")
 raw<-read.csv("raw_cn.csv")
+
+
+raw$male<-ifelse(raw$gender=="Man (cisgender)"|raw$gender=="Man (transgender)",1,0)
+raw$straight<-ifelse(raw$sex_orien=="Straight",1,0)
+raw$cisgendered<-ifelse(raw$gender=="Man (cisgender)"|raw$gender=="Woman (cisgender)",1,0)
+
+summary(raw$male)
+summary(raw$straight)
+summary(raw$cisgendered)
 

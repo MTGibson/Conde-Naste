d03d048b1a738b6496cd3f3b196d81ca4252e069
mikegibson93 <45637415+mikegibson93@users.noreply.github.com>
2020-06-13 18:35:14 -0400

    added some regressions to the cleaning code

--- 02_cleaning.R
+++ 02_cleaning.R
@@ -31,15 +31,31 @@ label(conde_nast$yrs_exp) <- "Years of experience, topcoded at 20"
 
 conde_nast$reports_ind<-ifelse(conde_nast$reports_ind=="NULL",0,conde_nast$reports_ind)
 conde_nast$reports_dir<-ifelse(conde_nast$reports_dir=="NULL",0,conde_nast$reports_dir)
+conde_nast$reports_any<-ifelse(conde_nast$reports_dir>0 | conde_nast$reports_ind>0,1,0)
+conde_nast$reports_total<-conde_nast$reports_dir+conde_nast$reports_ind
 
+summary(conde_nast$white)
+summary(conde_nast$male)
+summary(conde_nast$straight)
+summary(conde_nast$cisgendered)
+summary(conde_nast$salary)
+summary(conde_nast$reports_ind)
+summary(conde_nast$reports_dir)
+summary(conde_nast$reports_any)
+summary(conde_nast$reports_total)
 
 
 c<-ggplot(data=conde_nast, aes(salary))
-c1<-c+geom_density(kernel="gaussian")
+c1<-c+geom_density()
 c1
 
 c2<-c1+facet_grid(rows=vars(white_lb), cols=vars(male_lb))
 
 c2
+
+conde_nast$ln_salary<-ln(conde_nast$salary)
+
+model_1 <- lm(salary ~ white + male + straight + cisgendered+ yrs_exp+yrs_role,data=conde_nast)
+summary(model_1)
 
 

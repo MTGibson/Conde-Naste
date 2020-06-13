d83e97a0df52f494971262617f15f8810854dbe1
mikegibson93 <45637415+mikegibson93@users.noreply.github.com>
2020-06-13 11:43:53 -0400

    Graphed salaries by race and gender

--- 02_cleaning.R
+++ 02_cleaning.R
@@ -6,7 +6,7 @@ library('rvest')
 library('ggplot2')
 library('tidyverse')
 library('sjmisc')
-
+library('Hmisc')
 
 setwd("/Users/michaelgibson/Documents/GitHub/Conde-Naste")
 conde_nast<-read.csv("conde_nast.csv")
@@ -22,7 +22,13 @@ summary(conde_nast$cisgendered)
 summary(conde_nast$salary)
 
 conde_nast$yrs_role<-as.numeric(conde_nast$yrs_role)
-conde_nast$yrs_exp<-ifelse(conde_nast$yrs_exp=="20 or more",20,yrs_exp)
+conde_nast$yrs_exp<-ifelse(conde_nast$yrs_exp=="20 or more",20,conde_nast$yrs_exp)
+label(conde_nast$yrs_exp) <- "Years of experience, topcoded at 20"
+
+conde_nast$reports_ind<-ifelse(conde_nast$reports_ind=="NULL",0,conde_nast$reports_ind)
+conde_nast$reports_dir<-ifelse(conde_nast$reports_dir=="NULL",0,conde_nast$reports_dir)
+
+
 
 
 
